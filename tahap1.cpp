2538211be198f5aeac4fcdd50a8e7a567276393b
GalihPamungkas07 <32285690+GalihPamungkas07@users.noreply.github.com>
2018-01-20 15:11:09 +0700

    tahap awal

---
+++ tahap1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class AC{
+	public:
+		void unggul1 (string unggul1);
+		void unggul2 (string unggul2);
+		void unggul3 (string unggul3);
+		string tipe;
+		int harga;
+};
+
+int main(){
+	
+}
